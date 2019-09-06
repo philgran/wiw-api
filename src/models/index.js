3b5ca377bc054eb564fddc4482bb0a44310e233b
Phil Gran <philgran@gmail.com>
2019-09-06 15:59:49 -0500

    Adding configurations for heroku

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,23 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    dialect: 'postgres',
-  },
-);
+let sequelize = null
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+  );
+}
 
 const models = {
   User: sequelize.import('./user'),
